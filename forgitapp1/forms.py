7c71d332a746029824c95d7b9d5debd0055f8a8d
subrahmanyam0326 <subbu2698@gmail.com>
2021-01-27 12:00:23 +0530

    views and others commited

--- forgitapp1/forms.py
+++ forgitapp1/forms.py
@@ -2,10 +2,59 @@ from django import forms
 from .models import RegData,LogData
 
 class RegForm(forms.Form):
-    class Meta:
-        model=RegData
-        fields="__all__"
+    f_name=forms.CharField(
+        label="Enter Your First_Name",
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'first_name'
+            }
+        )
+    )
+    l_name=forms.CharField(
+        label="Enter Your Last_Name",
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'last_name'
+            }
+        )
+    )
+    uname=forms.CharField(
+        label="Enter Your Username",
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'username'
+            }
+        )
+    )
+    password=forms.CharField(
+        label="Enter Your password",
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'password'
+            }
+        )
+    )
+
 class LogForm(forms.Form):
-    class Meta:
-        model=LogData
-        fields="__all__"
+    uname=forms.CharField(
+        label="Enter Your Username",
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'username'
+            }
+        )
+    )
+    password=forms.CharField(
+        label="Enter Your password",
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'password'
+            }
+        )
+    )
--- forgitpro1/settings.py
+++ forgitpro1/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
         'NAME': 'dbb',
         'USER':'root',
         'PASSWORD':'root'
+
     }
 }
 
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR=os.path.join(BASE_DIR,'static')
